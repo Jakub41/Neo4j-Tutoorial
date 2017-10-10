e872c1aa3cbf1677cf99cb9fd89fb83f0f9931e6
Jakub41 <lemiszewski@gmx.com>
2017-10-10 11:28:51 +0200

    Neo4j added model for Asset

---
+++ app/models/asset.rb
@@ -0,0 +1,31 @@
+class RationalConverter
+  class << self
+    def primitive_type; String; end
+
+    def convert_type; Rational; end
+
+    def to_db(value)
+      "#{value.numerator}/#{value.denominator}"
+    end
+
+    def to_ruby(value)
+      value && Rational(value)
+    end
+    alias_method :call, :to_ruby
+  end
+  include Neo4j::Shared::Typecaster
+end
+
+class Asset
+  include Neo4j::ActiveNode
+
+  id_property :code
+  property :title, type: Integer
+  property :price, type: Rational
+
+  property :details
+  serialize :details
+
+  property :created_at
+  property :updated_at
+end
